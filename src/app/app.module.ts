87c8b6c30adbf21c8852c15c3d3bbd5f209fbeae
Elias Smith <goodanonim96@gmail.com>
2020-02-21 16:07:02 +0200

    Little fix.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,16 @@ import {RouterModule} from '@angular/router';
 import {MaterialModule} from './shared/material.module';
 import { UniversalTableComponent } from './components/universal-table/universal-table.component';
 import {CdkTableModule} from '@angular/cdk/table';
-import {MatCheckboxModule, MatExpansionModule, MatFormFieldModule, MatRadioModule} from '@angular/material';
 import { TablePreviewComponent } from './pages/table-preview/table-preview.component';
-import {MatCardModule, MatCheckboxModule, MatDatepickerModule, MatInputModule, MatRadioModule, MatSelectModule} from '@angular/material';
+import {
+  MatCardModule,
+  MatCheckboxModule,
+  MatDatepickerModule,
+  MatExpansionModule,
+  MatInputModule,
+  MatRadioModule,
+  MatSelectModule
+} from '@angular/material';
 import { UniversalInputsComponent } from './components/universal-inputs/universal-inputs.component';
 import {FormsModule} from '@angular/forms';
 import { AdvancedSearchComponent } from './components/advanced-search/advanced-search.component';
@@ -21,20 +28,21 @@ import { AdvancedSearchComponent } from './components/advanced-search/advanced-s
     UniversalInputsComponent,
     AdvancedSearchComponent,
   ],
-    imports: [
-        BrowserModule,
-        BrowserAnimationsModule,
-        RouterModule,
-        MaterialModule,
-        CdkTableModule,
-        MatCheckboxModule,
-        MatRadioModule,
-        MatCardModule,
-        MatInputModule,
-        FormsModule,
-        MatDatepickerModule,
-        MatSelectModule
-    ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    RouterModule,
+    MaterialModule,
+    CdkTableModule,
+    MatCheckboxModule,
+    MatRadioModule,
+    MatCardModule,
+    MatInputModule,
+    FormsModule,
+    MatDatepickerModule,
+    MatSelectModule,
+    MatExpansionModule
+  ],
   providers: [
   ],
   bootstrap: [AppComponent]
